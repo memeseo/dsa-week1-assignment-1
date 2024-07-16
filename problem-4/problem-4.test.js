04e57f35edc24aa8eb6842bcf8c9afcdb7e625e9
umbrellamoss <umbrellamoss@gmail.com>
2024-07-16 23:35:31 +0900

    Feat. problem-4

--- problem-4/problem-4.test.js
+++ problem-4/problem-4.test.js
@@ -1,4 +1,57 @@
-const solution = () => {
+//1. 가장 익숙한 방법으로 문제를 해결해 주세요.
+const solution1 = (binaryString) => {
+  const array = binaryString.split('').map(Number);
+  let result = 0;
+
+  for(let i = 0; i < array.length; i++){
+    const num = array.length - (i + 1);
+    result += array[i] * (2 ** num);
+  }
+ 
+  return result;
+ };
+
+// 2. 이번에는 재귀 함수로 문제를 해결해 주세요.
+const solution2 = (binaryString) => {
+
+  if (binaryString === '0') return 0;
+  if (binaryString === '1') return 1;
+
+  const array = binaryString.split('').map(Number);
+  const num = array[0] * ( 2 ** (array.length - 1));
+
+  return num + solution2(array.slice(1).join(''));
+};
+
+// 3. 꼬리 재귀 함수로 바꿔보세요.
+const solution3 = (binaryString, num = 0) => {
+
+  if(binaryString.length === 1) return num + Number(binaryString);
+  
+  const array = binaryString.split('').map(Number);
+  num += array[0] * (2 ** (array.length - 1));
+  
+  return solution3(array.slice(1).join(''), num);
+};
+
+// 4. 꼬리 재귀 최적화를 통해서 최적화해 보세요.
+const solution = (binaryString) => {
+  
+  let array = binaryString.split('').map(Number);
+  let result = 0;
+
+  while(true){
+    if (array.toString() === '0') {
+      return 0 + result;
+    }
+
+    if (array.toString() === '1') {
+      return 1 + result;
+    }
+
+    result += array[0] * (2 ** (array.length - 1));
+    array = array.slice(1);
+  }
 };
 
 test('10진수 숫자를 반환한다', () => {
